b734041ae4fb00de305f9224f0904d46cc72b624
Patrick Toepel <patrick.toepel@gmail.com>
2020-01-24 09:53:19 -0700

    coming soon

---
+++ app.js
@@ -0,0 +1,31 @@
+const countdown = document.querySelector('.countdown');
+
+const launchDate = new Date('Jan 1 2021 13:00:00').getTime();
+
+const interval = setInterval(() => {
+    const now = new Date().getTime();
+
+    const distance = launchDate - now;
+
+    const days = Math.floor(distance / (1000* 60 * 60 * 24));
+
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+
+    const mins = Math.floor((distance % (1000* 60 * 60))/ (1000 * 60));
+
+    const seconds = Math.floor((distance % (1000 * 60))/ 1000);
+
+    countdown.innerHTML = `
+    <div>${days} <span>Days</span></div>
+    <div>${hours} <span>Hours</span></div>
+    <div>${mins} <span>Mins</span></div>
+    <div>${seconds} <span>Secs</span></div>
+    `
+
+    if(distance < 0){
+        clearInterval(interval);
+        countdown.style.color = '#17a2b8';
+        countdown.innerHTML = "Launched!";
+    }
+
+}, 1000);
